3aeb44f1f2b04d17232072362d7bacb639a4ab45
h.jang <h.jang@xronos-inc.jp>
2024-11-06 10:22:16 +0900

    Initial Commit

---
+++ Test/TestApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestApp.swift
+//  Test
+//
+//  Created by ジャン ヒョンジュン on 2024/11/06.
+//
+
+import SwiftUI
+
+@main
+struct TestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TestTests/TestTests.swift
@@ -0,0 +1,17 @@
+//
+//  TestTests.swift
+//  TestTests
+//
+//  Created by ジャン ヒョンジュン on 2024/11/06.
+//
+
+import Testing
+@testable import Test
+
+struct TestTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
